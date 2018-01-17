185579d48eb45aa8b7ea84df4b089e57a76da4a5
derekpoon <thebevil@gmail.com>
2018-01-17 11:09:00 +0000

    working datepicker - can specify a default date other than today - can input and set date - selected date displayed in textview

--- app/src/main/java/com/derekpoon/simpledatepicker/DatePickerFragment.java
+++ app/src/main/java/com/derekpoon/simpledatepicker/DatePickerFragment.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.app.DialogFragment;
 import android.widget.DatePicker;
-
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by derekpoon on 17/01/2018.
@@ -42,11 +43,27 @@ public class DatePickerFragment extends DialogFragment implements DatePickerDial
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        // Use the current date as the default date in the picker
-        final Calendar c = Calendar.getInstance();
-        int year = c.get(Calendar.YEAR);
-        int month = c.get(Calendar.MONTH);
-        int day = c.get(Calendar.DAY_OF_MONTH);
+
+        Bundle bundle = getArguments();
+        String dobVal2 = bundle.getString("DOB","");
+        System.out.println(dobVal2);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MMM yyyy");
+        String dateInString = dobVal2;
+
+        Date date = null;
+        try {
+            date = sdf.parse(dateInString);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        System.out.println(year + " " + month + " " + day);
 
         // Create a new instance of DatePickerDialog and return it
         return new DatePickerDialog(getActivity(), this, year, month, day);
--- app/src/main/java/com/derekpoon/simpledatepicker/MainActivity.java
+++ app/src/main/java/com/derekpoon/simpledatepicker/MainActivity.java
@@ -1,16 +1,19 @@
 package com.derekpoon.simpledatepicker;
 
-import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
+import android.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-//be sure to implment the interface in the activity
+//be sure to implement the interface in the activity
 
 public class MainActivity extends AppCompatActivity implements DatePickerFragment.OnCompleteListener {
 
     private TextView displayDate;
+    private Button btnShowDatePicker;
+    String dobVal = "10 MAY 1980";
 
     public void onComplete(String selectedDate) {
         // After the dialog fragment completes, it calls this callback.
@@ -24,11 +27,25 @@ public class MainActivity extends AppCompatActivity implements DatePickerFragmen
         setContentView(R.layout.activity_main);
 
         displayDate = (TextView)findViewById(R.id.selected_date);
+        btnShowDatePicker = (Button)findViewById(R.id.button);
+        btnShowDatePicker.setOnClickListener(btnHandler);
 
     }
 
-    public void showDatePickerDialog(View v) {
-        DialogFragment newFragment = new DatePickerFragment();
-        newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
+    View.OnClickListener btnHandler = new View.OnClickListener() {
+        public void onClick(View v) {
+            DialogFragment picker = new DatePickerFragment();
+
+            Bundle bundle = new Bundle();
+            bundle.putString("DOB", dobVal);
+            picker.setArguments(bundle);
+            picker.show((MainActivity.this).getFragmentManager(),"datePicker");
+        }
+    };
+
+
+//    public void showDatePickerDialog(View v) {
+//        DialogFragment newFragment = new DatePickerFragment();
+//        newFragment.show(getSupportFragmentManager(), "datePicker");
+//    }
 }
